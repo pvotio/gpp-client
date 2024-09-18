d1c798137963d9be3f2a8db02b8d8f72442c5e4d
Ali Moghimi <mghmiali@gmail.com>
2024-09-18 13:08:57 +0000

    init: project

---
+++ config/settings.py
@@ -0,0 +1,13 @@
+from decouple import config
+
+LOG_LEVEL = config("LOG_LEVEL", default="INFO")
+OUTPUT_TABLE = config("OUTPUT_TABLE")
+INSERTER_MAX_RETRIES = config("INSERTER_MAX_RETRIES", default=3, cast=int)
+SFTP_HOST = config("SFTP_HOST")
+SFTP_PORT = config("SFTP_PORT", cast=int)
+SFTP_USER = config("SFTP_USER")
+SFTP_PASSWORD = config("SFTP_PASSWORD")
+MSSQL_SERVER = config("MSSQL_SERVER")
+MSSQL_DATABASE = config("MSSQL_DATABASE")
+MSSQL_USERNAME = config("MSSQL_USERNAME")
+MSSQL_PASSWORD = config("MSSQL_PASSWORD")
