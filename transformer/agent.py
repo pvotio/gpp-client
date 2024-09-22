7cf1382f5702ef70b11eead0982a55a5510d32af
Ali Moghimi <mghmiali@gmail.com>
2024-09-22 14:35:56 +0000

    feat: add transform logic

--- transformer/agent.py
+++ transformer/agent.py
@@ -3,5 +3,11 @@ import datetime
 import pandas as pd
 
 
-def transform(parsed_data):
-    pass
+def transform(raw_data):
+    for name, table in raw_data.items():
+        raw_data[name] = pd.concat(table, ignore_index=True)
+        raw_data[name]["timestamp_created_utc"] = datetime.now()
+        if "index" in raw_data[name].columns.to_list():
+            raw_data[name].drop(columns={"index"}, inplace=True)
+
+    return raw_data
